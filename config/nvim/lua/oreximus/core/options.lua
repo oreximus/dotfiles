e963ad86ae9aef5b9af0a82fa8a043cdb85a1367
oreximus <oreximus@gmail.com>
2024-04-14 14:15:57 +0530

    Updating nvim config

--- config/nvim/lua/oreximus/core/options.lua
+++ config/nvim/lua/oreximus/core/options.lua
@@ -16,6 +16,7 @@ opt.wrap = false
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.cursorline = true
 
 -- turn on termguicolors for tokyonight colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
@@ -33,3 +34,6 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 -- split windows
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true --split horizontal window to the bottom
+
+opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undodir"
+opt.undofile = true
